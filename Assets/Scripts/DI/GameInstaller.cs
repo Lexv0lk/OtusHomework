2e90ed020960c295e10336986e8df1fca44ddd4b
Lexv0lk <Lexvolk2004@yandex.ru>
2024-09-08 15:58:58 +0400

    добавил zenject, написал eventBus

---
+++ Assets/Scripts/DI/GameInstaller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace DI
+{
+    public class GameInstaller
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EventBus/EventBus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventBus
+{
+    public class EventBus
+    {
+        private readonly Dictionary<Type, IEventHandlerCollection> _handlers = new();
+        
+        public void Subscribe<TEvent>(Action<TEvent> handler)
+        {
+            var key = typeof(TEvent);
+
+            if (_handlers.ContainsKey(key) == false)
+                _handlers[key] = new EventHandlerCollection<TEvent>();
+            
+            _handlers[key].Subscribe(handler);
+        }
+        
+        public void Unsubscribe<TEvent>(Action<TEvent> handler)
+        {
+            var key = typeof(TEvent);
+            
+            if (_handlers.TryGetValue(key, out var handlers))
+                handlers.Unsubscribe(handler);
+        }
+        
+        public void RaiseEvent<TEvent>(TEvent evt)
+        {
+            var key = evt.GetType();
+
+            if (_handlers.TryGetValue(key, out var handlers))
+                handlers.RaiseEvent(evt);
+        }
+    }
+}
